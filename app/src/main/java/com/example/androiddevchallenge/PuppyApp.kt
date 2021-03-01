92decc98e171a9b46de69160daebef3c89ec99b2
Livin Lawrence <livinlawrence@gmail.com>
2021-03-01 07:59:40 +0200

    Spotless check done

--- app/src/main/java/com/example/androiddevchallenge/PuppyApp.kt
+++ app/src/main/java/com/example/androiddevchallenge/PuppyApp.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import androidx.compose.runtime.Composable
@@ -12,7 +27,6 @@ import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.ui.view.puppyDetails
 import com.example.androiddevchallenge.ui.view.puppyHome
 
-
 @Composable
 fun PuppyApp() {
     val navController = rememberNavController()
@@ -21,14 +35,15 @@ fun PuppyApp() {
         NavHost(
             navController,
             startDestination = PuppyHome
-        )
-        {
+        ) {
             composable(PuppyHome) { puppyHome(actions.showPuppyDetails) }
             composable(
                 "puppyDetail/{pos}",
-                arguments = listOf(navArgument(Routes.PuppyArgs.POSITION) {
-                    type = NavType.IntType
-                })
+                arguments = listOf(
+                    navArgument(Routes.PuppyArgs.POSITION) {
+                        type = NavType.IntType
+                    }
+                )
             ) { backStackEntry ->
                 backStackEntry.arguments?.getInt(Routes.PuppyArgs.POSITION)?.let {
                     puppyDetails(
--- app/src/main/java/com/example/androiddevchallenge/ui/view/PuppyHome.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/view/PuppyHome.kt
@@ -1,23 +1,37 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.Card
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,7 +46,6 @@ import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.Utils.getPuppies
 import com.example.androiddevchallenge.model.Puppy
 
-
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun puppyHome(showPuppyDetails: (Int) -> Unit) {
@@ -45,12 +58,11 @@ fun puppyHome(showPuppyDetails: (Int) -> Unit) {
         content = {
             val names: List<Puppy> = getPuppies()
             Column(modifier = Modifier.fillMaxHeight()) {
-                buildPuppiesList(names,showPuppyDetails, Modifier.weight(1f))
+                buildPuppiesList(names, showPuppyDetails, Modifier.weight(1f))
             }
         }
     )
 }
-
 
 @Composable
 fun buildPuppiesList(puppies: List<Puppy>, showPuppyDetails: (Int) -> Unit, modifier: Modifier = Modifier) {
@@ -66,7 +78,7 @@ fun buildPuppiesList(puppies: List<Puppy>, showPuppyDetails: (Int) -> Unit, modi
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
-                        .clickable(onClick = {showPuppyDetails(index)})
+                        .clickable(onClick = { showPuppyDetails(index) })
                 ) {
                     Image(
                         painter = painterResource(puppy.photo),
@@ -82,7 +94,7 @@ fun buildPuppiesList(puppies: List<Puppy>, showPuppyDetails: (Int) -> Unit, modi
                     Text(
                         text = puppy.breed,
                         style = TextStyle(
-                            fontSize = 30.sp, color = Color.DarkGray,fontFamily = FontFamily.Cursive
+                            fontSize = 30.sp, color = Color.DarkGray, fontFamily = FontFamily.Cursive
                         ),
                         textAlign = TextAlign.Center
                     )
@@ -91,4 +103,3 @@ fun buildPuppiesList(puppies: List<Puppy>, showPuppyDetails: (Int) -> Unit, modi
         }
     }
 }
-
